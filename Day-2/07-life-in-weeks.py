47097090db889d9746ca94a0c04d9c4dbe9b1649
sanghaarsha <sanghaarsha@gmail.com>
2020-11-12 21:10:50 +0545

    age calc

---
+++ Day-2/07-life-in-weeks.py
@@ -0,0 +1,9 @@
+print("Assuming we live upto 90 Years,\nThis program calculates your time left on Earth.\n")
+
+age = int(input("Enter your age: "))
+
+days = (90 - age)*365
+weeks = int(days/7)
+months = int(days/30)
+
+print(f"You have {days} days,{weeks} weeks,{months} months left.\n")
