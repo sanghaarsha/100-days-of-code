28ffcfb9c6db79c11d666b4a3d8ea69303595b25
sanghaarsha <sanghaarsha@gmail.com>
2020-11-12 20:33:12 +0545

    operators

---
+++ Day-2/03-math-operators.py
@@ -0,0 +1,15 @@
+#  In addition to sum, there are more mathematical operators in python:
+#  +  : add
+#  -  : subtract
+#  *  : multiply
+#  /  : divide
+#  % (modulo) : return remainders from divison
+# ** : power raised
+
+# Order of operations : PEMDAS (Parenthesis, Exponentsm Multiply, Divide, Add, Subtract)
+
+print(3 + 2)  # 5
+print(3 - 2)  # 1
+print(3 * 2)  # 6
+print(3 ** 2)  # 9
+print(3 % 2)  # 1
