9a11ba6b800b8df13d607efbe8d40d6a9d1a774a
sanghaarsha <sanghaarsha@gmail.com>
2020-11-12 20:58:05 +0545

    fstring

---
+++ Day-2/06-formatted-output-fstring.py
@@ -0,0 +1,11 @@
+name = "Josh"
+score = 5
+
+print("Hello " + name + ", Your score is " + str(score) + ".")
+
+# Doing type conversion for displaying ouput everytime is very time consuming
+# so, python provides formatted output : fstring
+
+print(f"Hello {name}, Your score is {score}.")
+
+# Code is lesser and less error prone
