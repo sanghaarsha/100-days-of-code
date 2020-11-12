1a274c8bf33bb88f6e7da7f0fad34b949454eed9
sanghaarsha <sanghaarsha@gmail.com>
2020-11-12 20:24:31 +0545

    type conversion

---
+++ Day-2/02-types-errors-&-conversion.py
@@ -0,0 +1,27 @@
+# TypeErrors are raised when the programmer fails to check the type of object before performing an operation on them.
+
+# For example adding a intger to the string
+# print(22+"hello")
+
+# To avoid we must check passed object are of same types
+# Python provides built in function to check types
+# type(data)
+
+# Type is shown like this : <class 'type'>
+
+print(type(22))  # <class 'int'>
+# int : means its an integer
+
+print(type("Hello"))  # <class 'str'>
+# str : means its an string
+
+
+# Lets do type conversion
+a = "123"  # string type
+b = 123.45  # float type
+
+# if we add them without conversion we will get an error
+# so lets convert a into float
+a = float(a)
+print(a+b)  # 246.45
+# numbers get added and printed in the terminal
