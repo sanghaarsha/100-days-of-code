875e73bfefd9d9aef0ed9edaa20031a0dfa85c9a
sanghaarsha <sanghaarsha@gmail.com>
2020-11-12 20:39:24 +0545

    BMI calculator

---
+++ Day-2/04-BMI-calculator.py
@@ -0,0 +1,7 @@
+weight = float(input("Enter your weight in KiloGram (kg):\n"))
+
+height = float(input("Enter your height in Meters (m):\n"))
+
+bmi = weight/(height**2)
+
+print("Your BMI is : " + str(bmi))
