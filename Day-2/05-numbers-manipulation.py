d11ed537942d3d76fc2ac6f30253aebf319d3679
sanghaarsha <sanghaarsha@gmail.com>
2020-11-12 20:52:39 +0545

    number manipulation

---
+++ Day-2/05-numbers-manipulation.py
@@ -0,0 +1,27 @@
+# Rounding off numbers
+div = 8/3
+
+# Displaying raw output
+print(div)  # 2.6666666666666665
+
+# Rounding off by converting into integer
+print(int(div))  # 2
+
+# Rounding off using round() function
+print(round(div))  # 3
+
+# Round off by Precision using round()
+print(round(div, 4))  # 2.6667
+
+# Using floor divison : //
+print(8//3)  # 2
+
+# multi-operators :
+# += : increment and then assign
+# -= : decrement and then assign
+# *= : multiply and then assign
+# /= : divide and then assign
+
+score = 0
+score += 1
+print(score)  # 1
